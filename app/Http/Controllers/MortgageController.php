f1b31625d96cb9e137a027288651099758e9017b
Miheretab Alemu <mihrtab@gmail.com>
2023-07-07 01:26:06 +0300

    add unit test for morgate calculator api input validity check

---
+++ app/Http/Controllers/MortgageController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Validator;
+
+class MortgageController extends Controller
+{
+    public function calculate(Request $request) {
+        $input = $request->all();
+
+        $validator = Validator::make($input, [
+            'amount' => 'required|numeric|min:0',
+            'interest' => 'required|numeric|min:0|max:100',
+            'term' => 'required|numeric|min:0',
+            'extra' => 'numeric|min:0',
+        ], [
+            'amount.required' => 'The Loan Amount is required.',
+            'interest.required' => 'The Annual Interest Rate is required.',
+            'term.required' => 'The Loan Term is required.',
+            'interest.min' => 'The Annual Interest Rate should be between 0 and 100.',
+            'interest.max' => 'The Annual Interest Rate should be between 0 and 100.',
+            'term.min' => 'The Loan Term should be greater than 0.',
+            'amount.min' => 'The Loan Amount should be greater than 0.'
+        ]);
+
+        if ($validator->fails() || count($validator->errors()) > 0) {
+            return response()->json(['error' => $validator->errors()], 400);
+        }
+
+        return response()->json([]);
+    }
+}
---
+++ tests/Feature/MortgageTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\Response;
+use Tests\TestCase;
+
+
+class MortgageTest extends TestCase
+{
+
+    public function test_calculator_invalid_get_method()
+    {
+
+        $response = $this->json('get', 'api/calculate-mortgage');
+
+        $response->assertStatus(Response::HTTP_METHOD_NOT_ALLOWED);
+    }
+
+    public function test_calculator_invalid_input_format1()
+    {
+        $payload = [
+            'amount' => -1,
+            'interest' => 5,
+            'term' => 2,
+            'extra' => 0
+        ];
+
+        $response = $this->json('post', 'api/calculate-mortgage', $payload);
+
+        $response->assertStatus(Response::HTTP_BAD_REQUEST);
+        $response->assertJsonStructure(['error' => ['amount']]);
+    }
+
+    public function test_calculator_invalid_input_format2()
+    {
+        $payload = [
+            'amount' => 10000,
+            'interest' => -5,
+            'term' => 0,
+            'extra' => 0
+        ];
+
+        $response = $this->json('post', 'api/calculate-mortgage', $payload);
+
+        $response->assertStatus(Response::HTTP_BAD_REQUEST);
+        $response->assertJsonStructure(['error' => ['interest']]);
+    }
+
+    public function test_calculator_valid_input()
+    {
+        $payload = [
+            'amount' => 10000,
+            'interest' => 5,
+            'term' => 2,
+            'extra' => 0
+        ];
+
+        $response = $this->json('post', 'api/calculate-mortgage', $payload);
+
+        $response->assertStatus(Response::HTTP_OK);
+        $response->assertJsonStructure([]);
+    }
+}
